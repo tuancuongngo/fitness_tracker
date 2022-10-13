c7dfa909c260cb612c0efd1d43a425dc682174e4
Justice Lin <justicelin5403@vt.edu>
2022-10-13 14:49:03 -0400

    Allow workouts to be added and deleted from respective titles

--- index.js
+++ index.js
@@ -3,14 +3,27 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const Workout = require("./models/workout"); // import workout schema
+const Title = require("./models/title"); // import title schema*******************************************
 
 // mongoose
 const mongoose = require("mongoose");
 const port = process.env.PORT || 5000; // Backend port
 const methodOverride = require("method-override");
+const { title } = require("process");
 
 // .env file
 require("dotenv").config();
+
+// ****************************************************
+// mongoose.connect(  
+//     process.env.ATLAS_URI || 'mongodb://localhost:27017/todolistDB',
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true,
+//     },
+// );
+// ****************************************************
+
 
 // db connection
 const uri = process.env.ATLAS_URI;
@@ -40,7 +53,7 @@ app.get("/workout/:id", async (req, res) => {
 });
 
 // New workout page
-app.get("/new", (req, res) => {
+app.get("/new", async (req, res) => {
     res.render("new");
 });
 
@@ -48,6 +61,7 @@ app.get("/new", (req, res) => {
 app.post("/workouts", async (req, res) => {
     const newWorkout = new Workout(req.body);
     await newWorkout.save();
+    await Title.findOneAndUpdate( {name: newWorkout.name}, { $push: { exercises: newWorkout } }, { upsert: true, new: true });
     res.redirect(`workout/${newWorkout._id}`); // Redirect to workout detail page
 });
 
@@ -68,7 +82,9 @@ app.put("/workout/:id", async (req, res) => {
 // Delete a workout
 app.delete("/workout/:id", async (req, res) => {
     const { id } = req.params;
-    const deleteWorkout = await Workout.findByIdAndDelete(id);
+    const workout = await Workout.findById(id);
+    await Workout.findByIdAndDelete(id);
+    await Title.findOneAndUpdate({ name: workout.name }, { $pull: { exercises: { _id: id } } }, { runValidators: true });
     res.redirect("/");
 });
 
